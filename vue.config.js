2449bbc899fe613c19fedbd7de0519eedc455a25
joren97x <sumagangjoren@gmail.com>
2024-03-26 12:22:11 +0000

    added publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   pluginOptions: {
+    publicPath: '/portfolio/',
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
